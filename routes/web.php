98bbeceffd83c700a62a723bf1ce43954de92cd3
abid-cyber <abidmustaghfirin25@gmail.com>
2025-04-20 18:38:47 +0700

    perbaiki routes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PasswordResetLinkController;
 use App\Http\Controllers\NewPasswordController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
 
 Route::get('/register', Register::class)->name('register');
