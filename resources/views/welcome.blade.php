df339ff332ebf460db18e2afc97b2137b0ba2541
Wahyu Darma <engineeringtrplA@gmail.com>
2025-04-21 21:57:33 +0700

    ubahbg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,16 @@
     <!-- Tambahan: Background gradient & animasi -->
 <style>
     body {
-        background: linear-gradient(to bottom right, #1e293b, #0f172a);
-    }
+    background: linear-gradient(135deg, #1e293b, #3b0764, #2563eb, #0f172a);
+    background-size: 400% 400%;
+    animation: gradientFlow 15s ease infinite;
+}
+@keyframes gradientFlow {
+    0% { background-position: 0% 50%; }
+    50% { background-position: 100% 50%; }
+    100% { background-position: 0% 50%; }
+}
+
 
     #mainTitle {
         opacity: 0;
