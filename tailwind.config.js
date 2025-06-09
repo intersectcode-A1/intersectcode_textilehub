16f02e137a8a1e43f83dff3375df52912e8b9048
Akbar Hidayatullah <akbar281804@gmail.com>
2025-06-09 11:36:32 +0700

    js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
     content: [
         "./resources/**/*.blade.php", "./resources/**/*.js", "./resources/**/*.vue",
     //   './resources/**/*.{html,js,php}', // Sesuaikan dengan struktur proyek kamu
